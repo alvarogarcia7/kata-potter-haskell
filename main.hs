89d4fd0c6a31852dcf50864284da4e3022f69fe3
Alvaro Garcia <derecerca@gmail.com>
2016-10-04 23:36:43 +0100

    WIP: recursive call - wrong

--- main.hs
+++ main.hs
@@ -1,14 +1,18 @@
 import Data.List
+import Debug.Trace
 
 dist :: [a]  -> [[a]]
 dist (x:[]) = [[x]]
 dist (x:xc) = [[x]] ++ dist xc
 
+subs :: Eq x => [x] -> [[[x]]]
+subs [] = [[[]]]
 subs x = map splitInSolutions $ nonEmptySubsequences x
   where
     nonEmptySubsequences =  tail . subsequences 
-    splitInSolutions serie = [serie, rest]
+    splitInSolutions serie = map (\y -> (serie ++ head y)) $ subs rest
       where
         rest = (x \\ serie)
+    
 
 
